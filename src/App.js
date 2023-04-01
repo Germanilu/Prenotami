ad352851e43d4e5801b52b8f082e2756e6f9a89b
Luciano Germani <lucianogermani93@gmail.com>
2023-04-01 22:11:14 +0200

    Fix basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-
-import './App.css';
+import React from 'react';
+import './index.css';
+import Header from './Components/Header/index.jsx';
+import Home from './Components/Home/Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
-    <div>
+
+  <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+  </BrowserRouter>
       
-      <h1>Aiutami A Prenotare</h1>
-      <h2>Ti aiutiamo a prenotare un'appuntamento presso il tuo consolato</h2>
-      <p>Questo sito nasce per risolvere una delle problematiche che tutti noi espatriati dobbiamo affrontare ogni volta che vogliamo rinnovare i nostri documenti</p>
-      <p>Purtroppo è sempre più difficile riuscire a richiedere un'appuntamento sul sito del consolato a causa dei pochi appuntamenti che vengono messi a disposizione ogni giorno.</p>
-      <p>Questo servizio ha lo scopo di richiedere una prenotazione per il rinnovo della CIE o del passaporto presso il consolato</p>
-
-      <h3>Come funziona?</h3>
-      <p>Basterà fornire i dati di accesso al sito Prenot@mi. Email e password. Se vuoi sapere come cambiare la tua password clicca QUI. Una volta ricevuti i dati di accesso inizieremo la ricerca e verrai notificato tramite il metodo di contatto da te scelto</p>
-      <p>Con questo servizio ci auguriamo di poterti aiutare con la ricerca degli appuntamenti e liberare il tuo prezioso tempo per evitare di ripetere sempre la stessa azione e ottenere sempre la risposta "non ci sono appuntamenti disponibili in questo momento"</p>
-      <p>Il costo del servizio pari a 25€ verrà effettuato dal richiedente una volta che la prenotazione verrà effettuata correttamente</p>
-      <p>Metodo di pagamento: Paypal, Bizum, Crypto</p>
-
-      <button>Richiedi un'appuntamento</button>
-    </div>
   );
 }
 
