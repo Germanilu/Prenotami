d4a058c090ca13b0ad2770fc7d3948b7fa2b6693
Luciano Germani <lucianogermani93@gmail.com>
2023-04-02 12:55:59 +0200

    Feat: Header design

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,22 @@
+
+import './index.scss';
+import logo from '../../media/LOGO.jpg'
+import {useNavigate} from 'react-router-dom';
 function Header() {
+
+    const navigate = useNavigate();
+    
 return(
-    <div>
-        Header
+    <div className="header-design">
+        <div className="header">
+            <img src={logo} alt="Logo" className='logo'  onClick={() => navigate('/')}/>
+            <p onClick={() => navigate('/')}>Inizio</p>
+            <p>Come funziona</p>
+            <p onClick={() => navigate('/reserve')}>Richiedi il servizio</p>
+            <p>Prezzo</p>
+        </div>
     </div>
-)
+    )
 }
 
 
