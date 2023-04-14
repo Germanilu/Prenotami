4f7c3b2a5110867638e5f06b3135dc737720dc00
Luciano Germani <lucianogermani93@gmail.com>
2023-04-14 18:59:45 +0200

    Fix: Responsive

--- src/Components/ChooseService/index.jsx
+++ src/Components/ChooseService/index.jsx
@@ -3,26 +3,17 @@ import {useNavigate} from 'react-router-dom';
 
 const ChooseService = () => {
 
-    const navigate = useNavigate();
-
+const navigate = useNavigate();
 
     return(
         <div className="choose-service-design">
             <h1>Scegli il tipo di servizio</h1>
             <p>Noi ci occuperemo di prenotare l'appuntamento presso il consolato</p>
             <div className="service-button-container">
-
-            
-    
             <div className='reserve-button' onClick={() => navigate('/reserve-passport')}> Passaporto</div>
-            
-         
             <div className='reserve-button' onClick={() => navigate('/reserve-cie')}> Carta d'Identità</div>
                 </div>
             </div>
-        
-
-   
     )
 }
 
