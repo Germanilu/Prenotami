8b199691b93bfa8624072300f8be3ec8373321a8
Luciano Germani <lucianogermani93@gmail.com>
2023-04-14 19:04:37 +0200

    Fix: Footer media

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,9 +5,15 @@ function Footer(){
 
     return(
         <div className="footer-design">
+            <div>
             <p>Appuntami LLC.</p>
             <p>110 16th St, Ste 1400 #276</p>
             <p>Denver, Colorado 80202</p>
+            </div>
+            <div>
+            <p>Per info:</p>
+            <p>appuntami2023@gmail.com</p>
+            </div>
         </div>
     )
 }
