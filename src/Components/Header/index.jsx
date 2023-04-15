8927191f939637415fc8034e105f86ab1602f639
Luciano Germani <lucianogermani93@gmail.com>
2023-04-15 21:32:15 +0200

    Feat: Implement new navbar

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,24 +1,47 @@
 
 import './index.scss';
-import logo from '../../media/LOGO.jpg'
 import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
 
 function Header() {
 
 const navigate = useNavigate();
+const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+const [menu_class, setMenuClass] = useState("menu hidden");
+const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+const updateMenu = () =>{
+    if(!isMenuClicked) {
+        setBurgerClass("burger-bar clicked")
+        setMenuClass("menu visible")
+    }
+    else {
+        setBurgerClass("burger-bar unclicked")
+        setMenuClass("menu hidden")
+    }
+    setIsMenuClicked(!isMenuClicked)
+}
 
 
 return(
     <div className="header-design">
-        <div className="header">
-            <img src={logo} alt="Logo" className='logo'  onClick={() => navigate('/')}/>
-            <p onClick={() => navigate('/')}>Inizio</p>
-            <a href='#howWork'>Come funziona</a>
-            <p onClick={() => navigate('/choose-service')}>Richiedi il servizio</p>
-            <a href='#price'>Prezzo</a>
+        <nav>
+            <div className='burger-menu' onClick={updateMenu}>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+                <div className={burger_class}></div>
+            </div>
+        </nav>
+        <div className={menu_class}>
+            <div className="menu-container">
+                <p onClick={() =>{ navigate('/'); updateMenu()}}>Inizio</p>
+                <a href='#howWork' onClick={() => { navigate('/'); updateMenu()}}>Come funziona</a>
+                <p onClick={() => {navigate('/choose-service');updateMenu()}}>Richiedi il servizio</p>
+                <a href='#price'  onClick={() => { navigate('/'); updateMenu()}}>Prezzo</a>
+            </div>
         </div>
     </div>
-    )
+)
 }
 
 
